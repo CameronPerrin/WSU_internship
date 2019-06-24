b40300d6875aafc79d0969abeaf0e35aa19b6256
stavnash <35539039+stavnash@users.noreply.github.com>
2019-06-24 13:02:59 -0400

    Phase 3 commit
    Phase 3: Dissection
    Added Stephen folder with all assets for the Phase3_New scene. Contains scripts, materials, shader, etc. The working scene is Phase3_New.

---
+++ CorrectWSUProject/WSUProjectCorrect/Assets/Stephen/Scripts/IgnoreRaycast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreRaycast : MonoBehaviour
+{
+    public GameObject obj;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.gameObject.tag == "Fatberg")
+        {
+            Debug.Log("Entered");
+            hit.gameObject.layer = 2;
+            obj = hit.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider hit)
+    {
+        if (hit.gameObject.tag == "Fatberg")
+        {
+            hit.gameObject.layer = 0;
+            obj = hit.gameObject;
+        }
+    }
+}
