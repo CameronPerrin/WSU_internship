e0e28138709defe4d1947cb46a1fc72771c68448
Gregory <31873199+YoungKrug@users.noreply.github.com>
2019-07-03 16:26:23 -0400

    Phase 3 stuff

---
+++ ARTest3/Assets/Scripts/ScavangerHuntScripts/ObjectInformation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInformation : MonoBehaviour
+{
+    public SpawnedInformation objectInfo;
+    public string name;
+    public string description;
+    public bool flushable; 
+    void Awake()
+    {
+        objectInfo.name = name;
+        objectInfo.information = description;
+        objectInfo.flushable = flushable;
+    }
+}
