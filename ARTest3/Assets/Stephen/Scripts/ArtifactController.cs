ef990e91186728df3480485f6ab5774da4e75b75
stavnash <35539039+stavnash@users.noreply.github.com>
2019-07-08 19:08:35 -0400

    Throw Mechanic Scripts and Test Scene
    Added throw mechanic and collision scripts.

--- ARTest3/Assets/Stephen/Scripts/ArtifactController.cs
+++ ARTest3/Assets/Stephen/Scripts/ArtifactController.cs
@@ -8,8 +8,12 @@ public class ArtifactController : MonoBehaviour
 
     public Color32 color = new Color32(244, 250, 107, 255);
     public Color32 origColor;
-    public GameObject clicked;
+    private GameObject clicked;
     public bool other;
+
+    public float distance;
+    public float throwForce;
+    public Transform cam;
 
     public Text textBox;
 	public Text nameBox;
@@ -23,6 +27,15 @@ public class ArtifactController : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown("space") && clicked && clicked.transform.parent != null)
+        {
+            clicked.GetComponent<Rigidbody>().isKinematic = false;
+            clicked.transform.parent = null;
+            clicked.GetComponent<Rigidbody>().AddForce(cam.forward * throwForce);
+            clicked.GetComponent<MeshRenderer>().material.color = origColor;
+            clicked = null;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -30,26 +43,36 @@ public class ArtifactController : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, 100.0f))
             {
-                if (other && hit.transform.gameObject != clicked)
+                //Check if other item has been clicked, drop current item and replace with new item
+                if (other && hit.transform.gameObject != clicked && clicked != null)
                 {
                     clicked.GetComponent<MeshRenderer>().material.color = origColor;
+                    clicked.transform.parent = null;
+                    clicked.GetComponent<Rigidbody>().isKinematic = false;
                 }
                 clicked = hit.transform.gameObject;
 
+                //If item not "highlighted" save it original color to revert back when needed (also note that an item has been clicked)
                 if (hit.transform.gameObject.GetComponent<MeshRenderer>().material.color != color)
                 {
                     origColor = hit.transform.gameObject.GetComponent<ItemInfo>().origColor;
                     other = true;
                 }
 
+                //Check if something is hit and is not "highlighted" then change its color to "highlighted" and parent to camera
+                //Else if clicked off current item, revert color to it's original, unparent, and change isKinematic back to false
                 if (hit.transform != null && hit.transform.gameObject.GetComponent<MeshRenderer>().material.color == origColor)
                 {     
                     //Debug statement to check click is working               
                     //PrintName(hit.transform.gameObject);
                     hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = color;
-                    textBoxContainer.SetActive(true);
-                    nameBox.text = hit.transform.gameObject.GetComponent<ItemInfo>().itemName;
-                    textBox.text = hit.transform.gameObject.GetComponent<ItemInfo>().desc;
+                    clicked.GetComponent<Rigidbody>().isKinematic = true;
+                    clicked.transform.position = Camera.main.transform.position + Camera.main.transform.forward * distance;
+                    clicked.transform.parent = Camera.main.transform;
+                    
+                    //textBoxContainer.SetActive(true);
+                    //nameBox.text = hit.transform.gameObject.GetComponent<ItemInfo>().itemName;
+                    //textBox.text = hit.transform.gameObject.GetComponent<ItemInfo>().desc;
                 }
                 else
                 {
@@ -63,9 +86,11 @@ public class ArtifactController : MonoBehaviour
                     }
                     */
                     clicked.GetComponent<MeshRenderer>().material.color = origColor;
-                        textBoxContainer.SetActive(false);
-                        nameBox.text = " ";
-                        textBox.text = " ";
+                    clicked.transform.parent = null;
+                    clicked.GetComponent<Rigidbody>().isKinematic = false;
+                    //textBoxContainer.SetActive(false);
+                    //nameBox.text = " ";
+                    //textBox.text = " ";
                 }
             }
             else
@@ -79,10 +104,16 @@ public class ArtifactController : MonoBehaviour
                     textBox.text = " ";
                 }
                 */
-                clicked.GetComponent<MeshRenderer>().material.color = origColor;
-                textBoxContainer.SetActive(false);
-                nameBox.text = " ";
-                textBox.text = " ";
+                //If no item is clicked, but one was previously clicked, revert to original color, unparent, and set isKinematic back to false
+                if(clicked)
+                {
+                    clicked.GetComponent<MeshRenderer>().material.color = origColor;
+                    clicked.transform.parent = null;
+                    clicked.GetComponent<Rigidbody>().isKinematic = false;
+                }
+                //textBoxContainer.SetActive(false);
+                //nameBox.text = " ";
+                //textBox.text = " ";
             }  
         }
     }
