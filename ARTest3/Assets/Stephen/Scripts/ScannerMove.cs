9591bc41003d7226f34ad4d431fecc3d94a3caf8
Gregory <31873199+YoungKrug@users.noreply.github.com>
2019-07-15 11:59:34 -0400

    Small push of things

---
+++ ARTest3/Assets/Stephen/Scripts/ScannerMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScannerMove : MonoBehaviour
+{
+    private float x;
+    private float y;
+    private Vector3 rotate;
+    public Transform cam;
+
+    //Test Scanner Beep
+    public Text textBox;
+    public Text itemName;
+
+    void Start()
+    {
+        cam = Camera.main.transform;    
+    }
+
+    void Update()
+    {
+        y = Input.GetAxisRaw("PS X");
+        x = Input.GetAxisRaw("PS Y");
+        rotate = new Vector3(x * -1, y * -1, 0);
+        transform.eulerAngles = transform.eulerAngles - rotate;
+
+        Ray ray = new Ray(cam.position, cam.forward);
+        Debug.DrawRay(ray.origin,ray.direction,Color.blue);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            Debug.Log("Hit Sum'n");
+            Debug.Log(hit.transform.gameObject);
+            textBox.text = "!";
+
+            if (Input.GetButtonDown("Submit"))
+            {
+                itemName.text = hit.transform.gameObject.name;
+            }
+        }
+        else
+        {
+            textBox.text = ".";
+            itemName.text = "";
+        }
+    }
+}
