6e634461a42c5d9e0b42fac49edf9d38e512a7e6
stavnash <35539039+stavnash@users.noreply.github.com>
2019-07-18 20:37:42 -0400

    Added functionality to scanner script+
    Added to scanner script and created a new scene for sewer phase

--- ARTest3/Assets/Stephen/Scripts/ScannerMove.cs
+++ ARTest3/Assets/Stephen/Scripts/ScannerMove.cs
@@ -11,6 +11,14 @@ public class ScannerMove : MonoBehaviour
     public Transform cam;
     public float distance;
     public float shift;
+    public float shiftV;
+
+    //Distance list etc.
+    public List<Vector3> distances;
+    public GameObject one;
+    public GameObject two;
+    public GameObject three;
+    public float away;
 
     //Test Scanner Beep
     public Text indicator;
@@ -20,11 +28,20 @@ public class ScannerMove : MonoBehaviour
     public GameObject textBoxContainer;
     public GameObject clicked;
     private Vector3 oldPos;
+    private Vector3 newPos;
+
+    public float speed;
+    private float startTime;
+    public float length;
+
     private Quaternion oldRot;
 
     void Start()
     {
-        cam = Camera.main.transform;    
+        cam = Camera.main.transform;
+        distances.Add(one.transform.position);
+        distances.Add(two.transform.position);
+        distances.Add(three.transform.position);
     }
 
     void Update()
@@ -46,11 +63,25 @@ public class ScannerMove : MonoBehaviour
 
             if (Input.GetButtonDown("Submit") && !clicked)
             {
+                
                 clicked = hit.transform.gameObject;
                 oldPos = clicked.transform.position;
                 oldRot = clicked.transform.rotation;
                 clicked.transform.parent = cam;
-                clicked.transform.position = Camera.main.transform.position + Camera.main.transform.forward * distance  + Camera.main.transform.right * shift;
+
+                
+                newPos = Camera.main.transform.position + Camera.main.transform.forward * distance 
+                                            + Camera.main.transform.right * shift 
+                                            + Camera.main.transform.up * shiftV;
+                /*
+                startTime = Time.time;
+                length = Vector3.Distance(oldPos, newPos);
+                float distCovered = (Time.time - startTime) * speed;
+                float fracJourney = distCovered / length;
+                clicked.transform.position = Vector3.Lerp(clicked.transform.position, newPos, fracJourney);
+                */
+
+                clicked.transform.position = newPos;
                 clicked.transform.rotation = new Quaternion(0,0,0,0);
                 textBoxContainer.SetActive(false);
                 descContainer.SetActive(true);
@@ -61,10 +92,26 @@ public class ScannerMove : MonoBehaviour
         else
         {
             indicator.text = ".";
+            //Not currently working
+            //foreach(var item in distances)
+            //{
+                if(Vector3.Distance(distances[0], ray.GetPoint(25)) < 3f || Vector3.Distance(distances[1], ray.GetPoint(25)) < 3f || Vector3.Distance(distances[2], ray.GetPoint(25)) < 3f)
+                {   
+                    //away = Vector3.Distance(distances[0], ray.GetPoint(25));
+                    indicator.color = Color.red;
+                } 
+                else
+                {
+                    //away = Vector3.Distance(distances[0], ray.GetPoint(25));
+                    indicator.color = Color.white;
+                }
+            //}
         }
+        
+        
 
         if (Input.GetButtonDown("Cancel") && clicked)
-            {
+        {
                 clicked.transform.parent = null;
                 clicked.transform.position = oldPos;
                 clicked.transform.rotation = oldRot;
@@ -73,6 +120,11 @@ public class ScannerMove : MonoBehaviour
                 descContainer.SetActive(false);
                 itemName.text = "";
                 desc.text = "";
-            }
+        }
+    }
+
+    private void MoveToFront()
+    {
+    
     }
 }
