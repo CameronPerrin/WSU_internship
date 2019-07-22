570f541ebf6053f559ac02a596a806c69ddff91d
Gregory <31873199+YoungKrug@users.noreply.github.com>
2019-07-22 18:47:24 -0400

    Added HDRP and Timer

--- ARTest3/Assets/Scripts/ScavangerHuntScripts/ScavangerHuntScripts/Timer.cs
+++ ARTest3/Assets/Scripts/ScavangerHuntScripts/ScavangerHuntScripts/Timer.cs
@@ -6,17 +6,28 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float time;
+    public float endTime;
+    [HideInInspector]
     public Text text;
+    public Slider slider;
 
     void Start()
     {
-        
+        slider.maxValue = endTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        slider.value += Time.deltaTime;
     }
+    public float GetSliderVal()
+    {
+        return slider.value;
+    }
+    public void AddSliderVal(float val)
+    {
+        slider.value += val;
+    }
+    
 }
