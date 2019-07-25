6d1c1523fc1ecd2c25082464f69c5f9bc59075e9
vsorensen <52054094+vsorensen@users.noreply.github.com>
2019-07-24 21:19:41 -0400

    Worked on Panel2 MiniGame
    InitialTest Push with Atom assist.  Changes were made to the Panel2MiniGame

---
+++ ARTest3/Assets/Vince_Game/NewJoystick.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewJoystick : MonoBehaviour
+{
+    
+
+    public int winningPosition;
+    public int Successes = 0;
+    [SerializeField] private float PositionAngle;
+
+    public AudioClip bell;
+    public AudioClip victory;
+    public AudioSource playerSource;
+    private bool played = false;
+
+    public float x;
+    public float y;
+    [SerializeField] public GameObject joystick;
+    private Vector3 rotate;
+
+
+    [SerializeField] private KeyCode check;
+
+    //these will target objects that have the rings of leds.
+    [SerializeField] public GameObject success0;
+    [SerializeField] public GameObject success1;
+    [SerializeField] public GameObject success2;
+
+
+
+    void Start()
+    {
+        GenWinning();
+    }
+    
+    private void Update()
+    {
+        Leds();
+
+        //joystick input
+        y = Input.GetAxisRaw("PS X");
+        x = Input.GetAxisRaw("PS Y");
+
+        //Gets the angle of the joystick.  Generates rotate
+        CurrentPosition();
+
+        //rotate image
+        joystick.transform.eulerAngles = rotate; 
+
+        //if the angle is within range of the winning angle plays notification sound
+        if (Mathf.Abs(PositionAngle - winningPosition) < 3 && played == false)
+        {
+            StartCoroutine(PlaySound());
+        }
+
+        //check to see if on the winning angle
+        if (Input.GetKeyDown(check) && PositionAngle == winningPosition)
+        {
+            Successes += 1;
+            GenWinning();
+            StartCoroutine(PlaySuccess());
+        }
+
+        //check if you have won 3 times
+        if (Successes == 3)
+        {
+            playerSource.PlayOneShot(victory);
+            //load other scene
+        }
+    }
+
+    void CurrentPosition()
+    {
+        //Depending on how many wins there are this will change the snapping to deviding the 360 degrees
+        switch (Successes)
+        {
+            case 0:
+                rotate = new Vector3(0, 0, Mathf.Round(Mathf.Rad2Deg * Mathf.Atan(x / y)));
+                PositionAngle = Mathf.Round(Mathf.Rad2Deg * Mathf.Atan(x / y) / 45f);
+                break;
+
+            case 1:
+                rotate = new Vector3(0, 0, Mathf.Round(Mathf.Rad2Deg * Mathf.Atan(x / y)));
+                PositionAngle = Mathf.Round(Mathf.Rad2Deg * Mathf.Atan(x / y) / 22.5f);
+                break;
+
+            case 2:
+                rotate = new Vector3(0, 0, Mathf.Round(Mathf.Rad2Deg * Mathf.Atan(x / y)));
+                PositionAngle = Mathf.Round(Mathf.Rad2Deg * Mathf.Atan(x / y) / 15f);
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    void GenWinning ()
+    {
+        switch (Successes)
+        {
+            case 0:
+                winningPosition = Random.Range(1, 8);
+                break;
+
+            case 1:
+                winningPosition = Random.Range(1, 16);
+                break;
+
+            case 2:
+                winningPosition = Random.Range(1, 24);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void Leds()
+    {
+        switch (Successes)
+        {
+            case 0:
+                success0.SetActive(true);
+                success1.SetActive(false);
+                success2.SetActive(false);
+                break;
+
+            case 1:
+                success0.SetActive(false);
+                success1.SetActive(true);
+                success2.SetActive(false);
+                break;
+
+            case 2:
+                success0.SetActive(false);
+                success1.SetActive(false);
+                success2.SetActive(true);
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    IEnumerator PlaySound()
+    {
+        playerSource.volume =  1/(Mathf.Abs(PositionAngle - winningPosition));
+
+        playerSource.PlayOneShot(bell);
+        played = true;
+        playerSource.volume = 1;
+        yield return new WaitForEndOfFrame();
+    }
+
+    IEnumerator PlaySuccess()
+    {
+
+        playerSource.PlayOneShot(victory);
+        yield return new WaitForEndOfFrame();
+    }
+}
+
+
