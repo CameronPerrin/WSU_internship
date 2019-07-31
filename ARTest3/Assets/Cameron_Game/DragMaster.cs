4c1f2b4dd21fc0907c33a6acd1245679459c96eb
Cameron Perrin <perrin.cameron@gmail.com>
2019-07-31 19:15:43 -0400

    Started work on a new drag and drop minigame with collision triggers for a selector

---
+++ ARTest3/Assets/Cameron_Game/DragMaster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragMaster : MonoBehaviour
+{
+    
+    public Transform dropable;
+    public Transform selector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Instantiate(dropable, new Vector3(-7,0,0), Quaternion.identity);
+        Instantiate(selector, new Vector3(0, 0, 0), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ARTest3/Assets/Cameron_Game/PickUpMaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpMaster : MonoBehaviour
+{
+    private bool hovered = false;
+    public int speed;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Debug.Log("OnCollisionEnter2D");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
