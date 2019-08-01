4db83cb58419b24239c453e194df2572739c4fe0
Cameron Perrin <perrin.cameron@gmail.com>
2019-08-01 00:01:02 -0400

    added a win condition to the drag and drop

---
+++ ARTest3/Assets/Cameron_Game/BoardMaster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardMaster : MonoBehaviour
+{
+    private GameObject toBeReplaced;
+
+    /*
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "pickUp")
+        {
+            toBeReplaced = GameObject.Find(this.name);
+            Debug.Log(toBeReplaced.name);
+            //Object.Destroy(toBeReplaced);
+
+
+        }
+    }
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
