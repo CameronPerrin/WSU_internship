9152fceea3968769e4d737cbb018972031064769
YoungKrug <gregjohn456@gmail.com>
2019-08-12 17:57:55 -0400

    Added CellPhone Stuff

---
+++ ARTest3/Assets/AutoTypeText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoTypeText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cellPhone;
+    public float typeDelay;
+    public string textToType;
+    bool isWaiting = false;
+    int length = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isWaiting)
+        {
+            if (length <= textToType.Length - 1)
+                StartCoroutine(Delay());
+            else
+            {
+                cellPhone.GetComponent<CellPhone>().playerHasSeenTextMessage = true;
+                isWaiting = true;
+            }
+            //else do nothing its done
+        }
+    }
+    IEnumerator Delay()
+    {
+        isWaiting = true;
+        GetComponent<Text>().text += textToType[length];
+        length++;
+        yield return new WaitForSeconds(typeDelay);
+        isWaiting = false;
+    }
+    public void RestartAutoType(string text)
+    {
+        textToType = text;
+        isWaiting = false;
+        length = 0;
+    }
+}
--- ARTest3/Assets/CellPhone.cs
+++ ARTest3/Assets/CellPhone.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 
 public class CellPhone : MonoBehaviour
 {
-    public GameObject canvas;
+    public GameObject textMessage;
     public GameObject player;
     public float closeEnoughDist;
-    public Animation aniClip;
     bool rotate;
+    [HideInInspector]
+    public bool playerHasSeenTextMessage;
+    Vector2 normSize;
     // Update is called once per frame
     void Update()
     {
@@ -17,15 +19,31 @@ public class CellPhone : MonoBehaviour
         {
             SwitchRotate();
         }
-        if(CheckIfPlayerIsClose())
+        if (CheckIfPlayerIsClose() && !playerHasSeenTextMessage)
         {
-            if(Input.GetKeyDown(KeyCode.Z))
-            {              
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
                 rotate = false;
                 SwitchRotate();
-                //canvas.SetActive(true);
+                textMessage.SetActive(!textMessage.activeInHierarchy);
+                //playerHasSeenTextMessage = true;
+                StartCoroutine(StopScreenAni());
             }
         }
+        else if(playerHasSeenTextMessage)
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                textMessage.GetComponent<RectTransform>().sizeDelta = normSize;
+                textMessage.SetActive(false);
+            }
+                /*  if(textMessage.activeInHierarchy)
+                      textMessage.GetComponent<RectTransform>().sizeDelta = normSize;
+                  if(!CheckIfPlayerIsClose())
+                      textMessage.SetActive(false);*/
+        }
+
+
     }
     bool CheckIfPlayerIsClose()
     {
@@ -33,14 +51,32 @@ public class CellPhone : MonoBehaviour
         if (closeEnoughDist <= dist)
             return true;
         else
+        {
+            //textMessage.SetActive(false);
             return false;
+        }
     }
     void SwitchRotate()
     {
         GetComponent<Animator>().enabled = false;
     }
+    //Used to call the cell phone after you called it the first time
     public void CallPhone()
     {
+        //textMessage.GetComponent<RectTransform>().sizeDelta = normSize;
+        rotate = false;
         GetComponent<Animator>().enabled = true;
+        playerHasSeenTextMessage = false;
+    }
+    IEnumerator StopScreenAni()
+    {
+        normSize = textMessage.GetComponent<RectTransform>().sizeDelta;
+        Vector3 pos = new Vector2(-1.22f, 2.54f);
+        Debug.Log(pos);
+        textMessage.GetComponent<Animator>().enabled = true;
+        yield return new WaitForSeconds(textMessage.GetComponent<Animation>().clip.length);
+        textMessage.GetComponent<Animator>().enabled = false;
+        textMessage.GetComponent<RectTransform>().sizeDelta = new Vector2(132.51f, 111.61f);
+        textMessage.GetComponent<RectTransform>().localPosition = pos;
     }
 }
