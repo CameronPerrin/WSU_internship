16b4ae5c1cfc45c8e20786f419e8d518dc8286e6
Cameron Perrin <perrin.cameron@gmail.com>
2019-08-19 19:29:55 -0400

    added bumbing text to the main menu

---
+++ ARTest3/Assets/Art Assets/Main Menu Assets/bumb.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bumb : MonoBehaviour
+{
+    public float speed;
+    public bool shrink = false;
+    Vector3 temp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        temp = transform.localScale;
+
+        if ((temp.x < 73) && !shrink)
+        {
+            temp.x += speed;
+            temp.y += speed;
+        }
+        else
+        {
+            shrink = true;
+        }
+
+        if ((temp.x >= 63) && shrink)
+        {
+            temp.x -= speed;
+            temp.y -= speed;
+        }
+        else
+        {
+            shrink = false;
+        }
+
+
+
+        transform.localScale = temp;
+    }
+}
