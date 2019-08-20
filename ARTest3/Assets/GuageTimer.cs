4a00cd09e2962a02b287a4b209a5687ade4e2792
Gregory <31873199+YoungKrug@users.noreply.github.com>
2019-08-20 13:08:22 -0400

    Modifying scripts

--- ARTest3/Assets/GuageTimer.cs
+++ ARTest3/Assets/GuageTimer.cs
@@ -10,27 +10,36 @@ public class GuageTimer : MonoBehaviour
     public float time;
     float timeForFill;
     float timeInc;
+    public float offset;
     // Start is called before the first frame update
     void Start()
     {
         timeForFill = time * 0.01f; // Seconds...Since its 0 - 1 if we go by time .delta time
-       // arrow.transform.eulerAngles = new Vector3(arrow.transform.eulerAngles.x, arrow.transform.eulerAngles.y, 245);
+        // arrow.transform.eulerAngles = new Vector3(arrow.transform.eulerAngles.x, arrow.transform.eulerAngles.y, 245);
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeInc += Time.deltaTime;
-        float filled = meter.GetComponent<Image>().fillAmount;
+      //  float filled;
+         timeInc += Time.deltaTime;  
+      //  filled = meter.GetComponent<Image>().fillAmount + 0.15f;
         float percentage = timeInc / time;
         timeForFill = percentage;
         //float fillIncrease = timeForFill * Time.deltaTime;
-        meter.GetComponent<Image>().fillAmount = timeForFill;
-       
-        float zPer = 100f - (percentage * 100f);
-        Debug.Log(zPer + " : " + percentage);
+        //if(offset != 0)
+        //    meter.GetComponent<Image>().fillAmount = timeForFill + .15f;
+        //  else
+        if (offset != 0)
+        {
+            meter.GetComponent<Image>().fillAmount += (Time.deltaTime + 0.15f);
+            offset = 0;
+        }
+        else
+            meter.GetComponent<Image>().fillAmount += (Time.deltaTime);
+        float zPer = 100 - (percentage * 100f);
+        Debug.Log(zPer/100 + " : " + meter.GetComponent<Image>().fillAmount);
         arrow.GetComponent<Transform>().eulerAngles = new Vector3(arrow.transform.eulerAngles.x, arrow.transform.eulerAngles.y,
-            (zPer/100) * 245f);
-
+            (zPer/100f) * 245f);
     }
 }
